f69a112347023f660dca8c72635d69c9b0241330
Edith Gatwiri <edithgatwiri70@gmail.com>
2024-12-07 10:25:43 +0300

    modification

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Write your code here!
+//  Remove <main> with id "main"
+const main = document.getElementById("main");
+main.remove();
+
+//  Create a new <h1> element 
+const newHeader = document.createElement("h1");
+
+// Set the id of newHeader to "victory"
+newHeader.id = "victory";
+
+//  Set newHeader to include your name
+newHeader.textContent = "Edith is the champion";
+
+//  Append newHeader
+document.body.append(newHeader);
